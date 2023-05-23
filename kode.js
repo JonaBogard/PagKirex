f4723a444d9ee4f14ac56fb1cfdc17f1179964aa
unknown <jonatr21vr@gmail.com>
2023-05-22 18:21:37 -0600

    PagV 3.52

--- kode.js
+++ kode.js
@@ -3,16 +3,17 @@ let marker;
 let geocoder;
 let responseDiv;
 let response;
+const center= { lat: 19.724371571150346, lng: -98.97133855356842 };
 
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
     zoom: 10,
-    center: { lat: 19.724371571150346, lng: -98.97133855356842 }, 
+    center: center, 
     mapTypeControl: false,
   });
 
   const marker = new google.maps.Marker({
-    position: center,
+    position: center ,
     map: map
   });
 
